d0f74475a54a73cd4c74f2ca4b2bc906ac04fa22
ledney07 <ledney21@gmail.com>
2021-03-30 20:01:28 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import data from "./data";
+import SingleQuestion from "./components/SingleQuestion";
 
 function App() {
+  const [questions, setQuestion] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <div className="container">
+        <h3>Questions and answer login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion
+                key={question.id}
+                title={question.title}
+                info={question.info}
+              />
+            );
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,34 @@
+const question = [
+  {
+    id: 1,
+    title: "Do I have to allow the use of cookes",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil",
+  },
+  {
+    id: 2,
+    title: "There is new chef in town",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenet",
+  },
+  {
+    id: 3,
+    title: "Allow the use of cookes",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenet",
+  },
+  {
+    id: 4,
+    title: "We are the best team in the world",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenet",
+  },
+  {
+    id: 5,
+    title: "Can we tell the world next thing",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenet",
+  },
+];
+
+export default question;
